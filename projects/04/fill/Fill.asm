8f2a3bbd4c1af049658749f5c0f245ae868b0355
Alexander Cecile <alexander.m.cecile@gmail.com>
2019-03-08 19:39:39 -0500

    Finished project 4.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -21,28 +21,61 @@
 // Each row is represented by 32 consecutive 16-bit words
 // End address: 16384 + 256 * 32 = 16384 + 8192 = 24576 (5?)
 
-@COLOR
-M=-1
+// KEYBOARD
+// The ASCII code of the key which has been pressed appears at address 24576
 
-// Store current value
-@16384
+// START LOOP 1
+(LOOP1)
+
+// Store current address for screen
+@SCREEN
 D=A
 @CURR
 M=D
 
-// Store last value
+// Store last address for screen + 1
 @24576
 D=A
 @END
 M=D
 
-// Start loop
-(LOOP)
+@KBD
+D=M
+
+// If no key is pressed, don't fill
+@SETNOFILL
+D; JEQ
+
+// If a key is pressed, fill
+@SETFILL
+0; JMP
+
+// Set colour to 0 (no fill)
+(SETNOFILL)
+@COLOUR
+M=0
+// Start writing to addresses
+@LOOP2
+0;JMP
+
+// Set colour to -1 (fill)
+(SETFILL)
+@COLOUR
+M=-1
+// Start writing to addresses
+@LOOP2
+0;JMP
+
+
+// START LOOP 2
+(LOOP2)
 
 // Fill with appropriate color
-@COLOR
+@COLOUR
 D=M
-@CURR // These 3 lines are to use an address saved in memory
+
+// These 3 lines are to use an address saved in memory
+@CURR
 A=M
 M=D
 
@@ -53,11 +86,10 @@ MD=M+1
 @END
 D=M-D
 
-// Return to start of loop
-@LOOP
+// END LOOP 2
+@LOOP2
 D;JGT
 
-// Infinite end loop
-(END)
-@END
-0;JEQ
+// END LOOP 1
+@LOOP1
+0;JMP
